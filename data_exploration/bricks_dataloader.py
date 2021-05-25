380afc65ab79c8eef4601860de551d2d3c6051d3
ele20 <ele20@ic.ac.uk>
2021-05-25 12:32:44 +0100

    Make Galaxy Classification OOP - Extracting Initialisation Methods

--- data_exploration/bricks_dataloader.py
+++ data_exploration/bricks_dataloader.py
@@ -119,48 +119,69 @@ class Brick:
     'Represents all attributes necessary to classify all galaxies in a brick into their categories'
 
     def __init__(self, data):
+        # The init method calls other methods to initialise the brick, considered bad practice to do so but with
+        # over 25 attributes this looked more than messy
         self.data = data
         self.initialise_brick()
 
     def initialise_brick(self):
+        self.initialise_fluxes()
+        self.extinction_correction()
+        self.initialise_no_observations_maskbits()
+        self.initialise_gaia_magnitudes()
+        self.calculate_signal_to_noise()
+        self.south = None
+
+    def calculate_signal_to_noise(self):
+        # Get the Signal-to-noise in g, r, z, W1 and W2 defined as the flux per
+        # band divided by sigma (flux x sqrt of the inverse variance).
+        self.snr_g = self.flux_g * np.sqrt(self.flux_ivar_g)
+        self.snr_r = self.flux_r * np.sqrt(self.flux_ivar_r)
+        self.snr_z = self.flux_z * np.sqrt(self.flux_ivar_z)
+        self.snr_w1 = self.flux_w1 * np.sqrt(self.flux_ivar_w1)
+        self.snr_w2 = self.flux_w2 * np.sqrt(self.flux_ivar_w2)
+
+    def initialise_gaia_magnitudes(self):
+        # get the Gaia-based g-, b- and r-band MAGNITUDES.
+        self.gaia_g_mag = self.data.field('gaia_phot_g_mean_mag')
+        self.gaia_b_mag = self.data.field('gaia_phot_bp_mean_mag')
+        self.gaia_r_mag = self.data.field('gaia_phot_rp_mean_mag')
+        # Retrieving the maskbits for quasar detection and boolean for brick is in north
+        self.maskbits = self.data.field('maskbits')
+
+
+    def initialise_no_observations_maskbits(self):
+        self.nobs_g = self.data.field('nobs_g')
+        self.nobs_r = self.data.field('nobs_r')
+        self.nobs_z = self.data.field('nobs_z')
+
+    def initialise_fluxes(self):
         self.flux_g = self.data.field('flux_g')
         self.flux_r = self.data.field('flux_r')
         self.flux_z = self.data.field('flux_z')
         self.flux_w1 = self.data.field('flux_w1')
         self.flux_w2 = self.data.field('flux_w2')
-
         # getting predicted -band flux within a fiber of diameter 1.5 arcsec from this object in 1 arcsec Gaussian seeing
-
         self.fiberflux_g = self.data.field('fiberflux_g')
         self.fiberflux_r = self.data.field('fiberflux_r')
         self.fiberflux_z = self.data.field('fiberflux_z')
-
-        # correcting for extinction ---> divide by the transmission
-
-        self.mw_transmission_g = self.data.field('mw_transmission_g')
-        self.mw_transmission_r = self.data.field('mw_transmission_r')
-        self.mw_transmission_z = self.data.field('mw_transmission_z')
-        self.mw_transmission_w1 = self.data.field('mw_transmission_w1')
-        self.mw_transmission_w2 = self.data.field('mw_transmission_w2')
-
         # Retrurns Predicted -band flux within a fiber of diameter 1.5 arcsec from all sources at this location in 1 arcsec Gaussian seeing
         # Returns Inverse variance of FLUXES
-
         self.fibertotflux_g = self.data.field('fibertotflux_g')
         self.fibertotflux_r = self.data.field('fibertotflux_r')
         self.fibertotflux_z = self.data.field('fibertotflux_z')
-
         self.flux_ivar_g = self.data.field('flux_ivar_g')
         self.flux_ivar_r = self.data.field('flux_ivar_r')
         self.flux_ivar_z = self.data.field('flux_ivar_z')
         self.flux_ivar_w1 = self.data.field('flux_ivar_w1')
         self.flux_ivar_w2 = self.data.field('flux_ivar_w2')
 
-
-
-        self.extinction_correction()
-
     def extinction_correction(self):
+        self.mw_transmission_g = self.data.field('mw_transmission_g')
+        self.mw_transmission_r = self.data.field('mw_transmission_r')
+        self.mw_transmission_z = self.data.field('mw_transmission_z')
+        self.mw_transmission_w1 = self.data.field('mw_transmission_w1')
+        self.mw_transmission_w2 = self.data.field('mw_transmission_w2')
         # correcting for extinction ---> divide by the transmission
         self.flux_g = self.flux_g / self.mw_transmission_g
         self.flux_r = self.flux_r / self.mw_transmission_r
@@ -171,6 +192,9 @@ class Brick:
         self.fiberflux_g = self.fiberflux_g / self.mw_transmission_g
         self.fiberflux_r = self.fiberflux_r / self.mw_transmission_r
         self.fiberflux_z = self.fiberflux_z / self.mw_transmission_z
+
+    def set_south(self, south):
+        self.south = south
 
 
 for no, brickname in enumerate(bricknames_south_sample):
@@ -188,50 +212,23 @@ for no, brickname in enumerate(bricknames_south_sample):
 
     brick = Brick(data)
 
-    # Obtaining the flux in nano-maggies of g, r, z, W1 and W2 bands.
-    flux_g, flux_r, flux_z, flux_w1, flux_w2, fiberflux_g, fiberflux_r, fiberflux_z = get_extinction_corrected_fluxes(
-        data)
-
-    # This Flux is not corrected for extinction
-    fibertotflux_g, fibertotflux_r, fibertotflux_z, flux_ivar_g, flux_ivar_r, flux_ivar_z, flux_ivar_w1, flux_ivar_w2 = get_fluxes_not_extinction_corrected(
-        data)
-
-    # Get the number of pixels contributed to the central pixels in the bands
-    nobs_g = data.field('nobs_g')
-    nobs_r = data.field('nobs_r')
-    nobs_z = data.field('nobs_z')
-
-    # get the Gaia-based g-, b- and r-band MAGNITUDES.
-    gaia_g_mag = data.field('gaia_phot_g_mean_mag')
-    gaia_b_mag = data.field('gaia_phot_bp_mean_mag')
-    gaia_r_mag = data.field('gaia_phot_rp_mean_mag')
-
-    # Get the Signal-to-noise in g, r, z, W1 and W2 defined as the flux per
-    # band divided by sigma (flux x sqrt of the inverse variance).
-    snr_g = flux_g * np.sqrt(flux_ivar_g)
-    snr_r = flux_r * np.sqrt(flux_ivar_r)
-    snr_z = flux_z * np.sqrt(flux_ivar_z)
-    snr_w1 = flux_w1 * np.sqrt(flux_ivar_w1)
-    snr_w2 = flux_w2 * np.sqrt(flux_ivar_w2)
-
-    # Retrieving the maskbits for quasar detection and boolean for brick is in north
-    maskbits = data.field('maskbits')
 
     # Extracting Positions, and Object IDs
     ids = data.field('brickid')
     ra = data.field('ra')
     dec = data.field('dec')
     objid = data.field('objid')
-
     south = south_survey_is_south[np.where(brickid_south == brickid)]
-    south_array = np.full(shape=len(flux_g), fill_value=south)
+    south_array = np.full(shape=len(ids), fill_value=south)
 
     if len(south) > 0:
         south = south[0]
     else:
         south = True
 
-    target_type = np.zeros(len(flux_g))
+    brick.set_south(south)
+
+    target_type = np.zeros(len(ids))
 
     is_LRG = isLRG(gflux=flux_g, rflux=flux_r, zflux=flux_z, w1flux=flux_w1, w2flux=flux_w2,
                    zfiberflux=fiberflux_z, rfluxivar=flux_ivar_r, zfluxivar=flux_ivar_z, w1fluxivar=flux_ivar_w1,
