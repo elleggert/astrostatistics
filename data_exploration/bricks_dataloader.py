ad9d163adf18ba3c4b1c5eb72a7a8e8c861c0e5a
ele20 <ele20@ic.ac.uk>
2021-05-25 12:53:19 +0100

    Make Galaxy Classification OOP - Added Function comments

--- data_exploration/bricks_dataloader.py
+++ data_exploration/bricks_dataloader.py
@@ -70,51 +70,6 @@ for filename in os.listdir('/Volumes/Astrostick/bricks_data/south_test/'):
 df = pd.DataFrame(columns=['BrickID', 'ObjectID', 'RA', 'DEC', 'South', 'Target_type'])
 
 
-def get_extinction_corrected_fluxes(data):
-    flux_g = data.field('flux_g')
-    flux_r = data.field('flux_r')
-    flux_z = data.field('flux_z')
-    flux_w1 = data.field('flux_w1')
-    flux_w2 = data.field('flux_w2')
-
-    # getting predicted -band flux within a fiber of diameter 1.5 arcsec from this object in 1 arcsec Gaussian seeing
-
-    fiberflux_g = data.field('fiberflux_g')
-    fiberflux_r = data.field('fiberflux_r')
-    fiberflux_z = data.field('fiberflux_z')
-
-    # correcting for extinction ---> divide by the transmission
-
-    mw_transmission_g = data.field('mw_transmission_g')
-    mw_transmission_r = data.field('mw_transmission_r')
-    mw_transmission_z = data.field('mw_transmission_z')
-    mw_transmission_w1 = data.field('mw_transmission_w1')
-    mw_transmission_w2 = data.field('mw_transmission_w2')
-
-    # correcting for extinction ---> divide by the transmission
-    return flux_g / mw_transmission_g, \
-           flux_r / mw_transmission_r, \
-           flux_z / mw_transmission_z, \
-           flux_w1 / mw_transmission_w1, \
-           flux_w2 / mw_transmission_w2, \
-           fiberflux_g / mw_transmission_g, \
-           fiberflux_r / mw_transmission_r, \
-           fiberflux_z / mw_transmission_z
-
-
-def get_fluxes_not_extinction_corrected(data):
-    # Retrurns Predicted -band flux within a fiber of diameter 1.5 arcsec from all sources at this location in 1 arcsec Gaussian seeing
-    # Returns Inverse variance of FLUXES
-    return data.field('fibertotflux_g'), \
-           data.field('fibertotflux_r'), \
-           data.field('fibertotflux_z'), \
-           data.field('flux_ivar_g'), \
-           data.field('flux_ivar_r'), \
-           data.field('flux_ivar_z'), \
-           data.field('flux_ivar_w1'), \
-           data.field('flux_ivar_w2')
-
-
 class Brick:
     'Represents all attributes necessary to classify all galaxies in a brick into their categories'
 
@@ -125,16 +80,17 @@ class Brick:
         self.initialise_brick()
 
     def initialise_brick(self):
+        """Calls different Initialisation Methods"""
         self.initialise_fluxes()
-        self.extinction_correction()
         self.initialise_no_observations_maskbits()
         self.initialise_gaia_magnitudes()
+        self.extinction_correction()
         self.calculate_signal_to_noise()
         self.south = None
 
     def calculate_signal_to_noise(self):
-        # Get the Signal-to-noise in g, r, z, W1 and W2 defined as the flux per
-        # band divided by sigma (flux x sqrt of the inverse variance).
+        """Get the Signal-to-noise in g, r, z, W1 and W2 defined as the flux per
+        band divided by sigma (flux x sqrt of the inverse variance)."""
         self.snr_g = self.flux_g * np.sqrt(self.flux_ivar_g)
         self.snr_r = self.flux_r * np.sqrt(self.flux_ivar_r)
         self.snr_z = self.flux_z * np.sqrt(self.flux_ivar_z)
@@ -142,20 +98,22 @@ class Brick:
         self.snr_w2 = self.flux_w2 * np.sqrt(self.flux_ivar_w2)
 
     def initialise_gaia_magnitudes(self):
-        # get the Gaia-based g-, b- and r-band MAGNITUDES.
+        """Initialise the Gaia-based g-, b- and r-band MAGNITUDES."""
         self.gaia_g_mag = self.data.field('gaia_phot_g_mean_mag')
         self.gaia_b_mag = self.data.field('gaia_phot_bp_mean_mag')
         self.gaia_r_mag = self.data.field('gaia_phot_rp_mean_mag')
-        # Retrieving the maskbits for quasar detection and boolean for brick is in north
-        self.maskbits = self.data.field('maskbits')
-
 
     def initialise_no_observations_maskbits(self):
+        # Initialise the Number of observations for that brick
         self.nobs_g = self.data.field('nobs_g')
         self.nobs_r = self.data.field('nobs_r')
         self.nobs_z = self.data.field('nobs_z')
+        # Retrieving the maskbits for quasar detection and boolean for brick is in north
+        self.maskbits = self.data.field('maskbits')
 
     def initialise_fluxes(self):
+        """ Initialise the various fluxes, fiber fluxes and inverse variance of the fluxes in the different bands"""
+
         self.flux_g = self.data.field('flux_g')
         self.flux_r = self.data.field('flux_r')
         self.flux_z = self.data.field('flux_z')
@@ -177,6 +135,7 @@ class Brick:
         self.flux_ivar_w2 = self.data.field('flux_ivar_w2')
 
     def extinction_correction(self):
+        """ Retrieves the milky_way transmission and uses it to correct given fluxes for extinction"""
         self.mw_transmission_g = self.data.field('mw_transmission_g')
         self.mw_transmission_r = self.data.field('mw_transmission_r')
         self.mw_transmission_z = self.data.field('mw_transmission_z')
@@ -194,19 +153,26 @@ class Brick:
         self.fiberflux_z = self.fiberflux_z / self.mw_transmission_z
 
     def set_south(self, south):
+        # Initialises the boolean whether the given brick is south
         self.south = south
 
     def classify_galaxies(self):
+        """ Function goes through every object in the brick and defines whether it is of LRG, ELG or QSO type """
+
+        """ Returns: an np array with 0 indicating no galaxy type of interest, 1 = LRG; 2 = ELG, 3 = QSO """
         target_type = np.zeros(len(self.flux_g))
 
-        is_LRG = isLRG(gflux=self.flux_g, rflux=self.flux_r, zflux=self.flux_z, w1flux=self.flux_w1, w2flux=self.flux_w2,
+        is_LRG = isLRG(gflux=self.flux_g, rflux=self.flux_r, zflux=self.flux_z, w1flux=self.flux_w1,
+                       w2flux=self.flux_w2,
                        zfiberflux=self.fiberflux_z, rfluxivar=self.flux_ivar_r, zfluxivar=self.flux_ivar_z,
                        w1fluxivar=self.flux_ivar_w1, gaiagmag=self.gaia_g_mag, gnobs=self.nobs_g, rnobs=self.nobs_r,
-                       znobs=self.nobs_z, maskbits=self.maskbits, zfibertotflux=self.fibertotflux_z, primary=None, south=self.south)
+                       znobs=self.nobs_z, maskbits=self.maskbits, zfibertotflux=self.fibertotflux_z, primary=None,
+                       south=self.south)
 
         target_type[np.where(is_LRG == True)] = 1
 
-        is_ELG, is_ELGVLO = isELG(gflux=self.flux_g, rflux=self.flux_r, zflux=self.flux_z, w1flux=self.flux_w1, w2flux=self.flux_w2,
+        is_ELG, is_ELGVLO = isELG(gflux=self.flux_g, rflux=self.flux_r, zflux=self.flux_z, w1flux=self.flux_w1,
+                                  w2flux=self.flux_w2,
                                   gfiberflux=self.fiberflux_g, gsnr=self.snr_g, rsnr=self.snr_r, zsnr=self.snr_z,
                                   gnobs=self.nobs_g, rnobs=self.nobs_r, znobs=self.nobs_z,
                                   maskbits=self.maskbits, south=self.south)
@@ -214,7 +180,8 @@ class Brick:
         target_type[np.where(is_ELG == True)] = 2
         target_type[np.where(is_ELGVLO == True)] = 2
 
-        is_QSO = isQSO_cuts(gflux=self.flux_g, rflux=self.flux_r, zflux=self.flux_z, w1flux=self.flux_w1, w2flux=self.flux_w2,
+        is_QSO = isQSO_cuts(gflux=self.flux_g, rflux=self.flux_r, zflux=self.flux_z, w1flux=self.flux_w1,
+                            w2flux=self.flux_w2,
                             w1snr=self.snr_w1, w2snr=self.snr_w2, maskbits=self.maskbits,
                             gnobs=self.nobs_g, rnobs=self.nobs_r, znobs=self.nobs_z,
                             objtype=None, primary=None, optical=False, south=self.south)
@@ -222,7 +189,6 @@ class Brick:
         target_type[np.where(is_QSO == True)] = 3
 
         return target_type
-
 
 
 for no, brickname in enumerate(bricknames_south_sample):
@@ -257,8 +223,7 @@ for no, brickname in enumerate(bricknames_south_sample):
 
     target_type = brick.classify_galaxies()
 
-
-    #Process array
+    # Process array
     stacked_array = np.stack((ids, objid, ra, dec, south_array, target_type), axis=1)
     support_df = pd.DataFrame(stacked_array, columns=['BrickID', 'ObjectID', 'RA', 'DEC', 'South', 'Target_type'])
     support_df.drop(support_df[support_df.Target_type == 0].index, inplace=True)
