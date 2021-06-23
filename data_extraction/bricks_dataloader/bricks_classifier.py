e26a1d16d1e6308a78f783a7dc111e371e7d7d00
ele20 <ele20@ic.ac.uk>
2021-06-23 13:06:20 +0100

    Now apending all files to one large HDU Table, afterwards classifying it together, 15 bricks of random forest cut from 88 to 25 seconds, should become even faster for bigger quantities

--- data_extraction/bricks_dataloader/bricks_classifier.py
+++ data_extraction/bricks_dataloader/bricks_classifier.py
@@ -1,6 +1,7 @@
 import time
 
 from astropy.io import fits
+from astropy.table import Table, vstack
 import numpy as np
 import os
 import pandas as pd
@@ -67,6 +68,7 @@ for filename in os.listdir('/Volumes/Astrostick/bricks_data/south_test/'):
     brickn = brickn.replace(".fits", "")
     bricknames_south_sample.append(brickn)
 
+print(bricknames_south_sample[0])
 df_galaxy = pd.DataFrame(columns=['BrickID', 'RA', 'DEC', 'Target_type', 'Fitbits', 'Maskbits'])
 df_stars = pd.DataFrame(columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
 df_galaxy.to_csv('../../bricks_data/galaxy_catalogue_sample_profiling.csv', index=False)
@@ -75,19 +77,13 @@ df_stars.to_csv('../../bricks_data/stellar_catalogue_sample_profiling.csv', inde
 print(df_galaxy.head())
 print(df_stars.head())
 
-
-"""
-
-hdulistSingleBrick = fits.open(f'/Volumes/Astrostick/bricks_data/south_test/tractor-{bricknames_south_sample[0]}.fits')
+new = Table.read('tractor-0053m395.fits', format='fits')
 
 
-data = hdulistSingleBrick[1].data
-brick = Brick(data)
-"""
 for no, brickname in enumerate(bricknames_south_sample):
 
     # df = pd.read_csv('../bricks_data/galaxy_catalogue_sample.csv')
-
+    """
     brickid = brickid_south[np.where(brickname_south == brickname)]
     if len(brickid > 0):
         brickid = brickid[0]
@@ -95,8 +91,9 @@ for no, brickname in enumerate(bricknames_south_sample):
         brickid = 0
         # Check how there can be a brickname without corresponding id
 
-    """
+   
     if no == 0:
+    
 
         south = south_survey_is_south[np.where(brickid_south == brickid)]
         if len(south) > 0:
@@ -113,9 +110,23 @@ for no, brickname in enumerate(bricknames_south_sample):
         df_stars = df_stars.append(support_df)
         continue
     """
-    hdulistSingleBrick = fits.open(f'/Volumes/Astrostick/bricks_data/south_test/tractor-{brickname}.fits')
-    data = hdulistSingleBrick[1].data
 
+    t2 = Table.read(f'/Volumes/Astrostick/bricks_data/south_test/tractor-{brickname}.fits', format='fits')
+    new = vstack([new, t2])
+
+    """
+    nrows2 = t2[1].data.shape[0]
+    nrows1 = t1[1].data.shape[0]
+    nrows = nrows1 + nrows2
+    hdu = fits.BinTableHDU.from_columns(t1[1].columns, nrows=nrows)
+    for colname in t1[1].columns.names:
+        hdu.data[colname][nrows1:] = t2[1].data[colname]
+    fits.update('tractor-0053m395.fits', hdu.data, 1)
+    #header = fits.getheader(f'/Volumes/Astrostick/bricks_data/south_test/tractor-{brickname}.fits')
+    """
+    print("Brick progression ", time.time() - start)
+
+    """
     brick = Brick(data)
     south = south_survey_is_south[np.where(brickid_south == brickid)]
     if len(south) > 0:
@@ -139,7 +150,6 @@ for no, brickname in enumerate(bricknames_south_sample):
     
 
 
-    """ NOW CLASSIFYNIG STARS"""
     brick.initialise_brick_for_stellar_density()
 
     stars = brick.get_stellar_objects()
@@ -154,7 +164,7 @@ for no, brickname in enumerate(bricknames_south_sample):
     # ['BrickID', 'ObjectID','RA', 'DEC', 'South', 'Target_type']
 
     # df.to_csv('../bricks_data/galaxy_catalogue_sample_profiling.csv', index=False)
-    """
+    
     df_galaxy.to_csv('../../bricks_data/galaxy_catalogue_sample_profiling.csv', mode='a', index=False, header=False)
     df_stars.to_csv('../../bricks_data/stellar_catalogue_sample_profiling.csv',  mode='a', index=False, header=False)
 
@@ -166,10 +176,46 @@ for no, brickname in enumerate(bricknames_south_sample):
     break
     print(df_galaxy.shape)
     print(df_stars.shape)
-"""
+    """
 
     # print(" ===================== Brick", brickname, " complete=====================")
 
+hdu = fits.table_to_hdu(new)
+fits.update('tractor-0053m395.fits', hdu.data, 1)
+hdu = fits.open('tractor-0053m395.fits')
+print(hdu.info())
+data = hdu[1].data
+print(type(data))
+
+ra = data.field('ra')
+print(len(ra))
+brick = Brick(data)
+brick.initialise_brick_for_galaxy_classification(south=True)
+target_type = brick.classify_galaxies()
+
+# Process array
+stacked_array = np.stack((brick.ids, brick.ra, brick.dec, target_type, brick.maskbits, brick.fitbits), axis=1)
+support_df = pd.DataFrame(stacked_array, columns=['BrickID', 'RA', 'DEC', 'Target_type', 'Fitbits', 'Maskbits'])
+support_df.drop(support_df[support_df.Target_type == 0].index, inplace=True)
+df_galaxy = df_galaxy.append(support_df)
+print(df_galaxy.shape)
+df_galaxy = df_galaxy.astype({'BrickID': 'int32', 'Target_type': 'int8', 'Fitbits': 'int16', 'Maskbits': 'int16'})
+
+print(df_galaxy.groupby('Target_type').count())
+
+
+"""
+
+data = hdul1[1].data
+brick = Brick(data)
+brick.initialise_brick_for_galaxy_classification(south=True)
+target_type = brick.classify_galaxies()
+
+# Process array
+stacked_array = np.stack((brick.ids, brick.ra, brick.dec, target_type, brick.maskbits, brick.fitbits), axis=1)
+support_df = pd.DataFrame(stacked_array, columns=['BrickID', 'RA', 'DEC', 'Target_type', 'Fitbits', 'Maskbits'])
+support_df.drop(support_df[support_df.Target_type == 0].index, inplace=True)
+df_galaxy = df_galaxy.append(support_df)
 
 print()
 print("=============================== Classification South Completed ==================================")
@@ -185,7 +231,7 @@ df_galaxy.to_csv('../../bricks_data/galaxy_catalogue_sample_profiling.csv', mode
 df_stars.to_csv('../../bricks_data/stellar_catalogue_sample_profiling.csv',  mode='a', index=False, header=False)
 print(df_galaxy.groupby('Target_type').count())
 
-
+"""
 
 print("Time taken for 15 bricks: ", time.time() - start)
 
