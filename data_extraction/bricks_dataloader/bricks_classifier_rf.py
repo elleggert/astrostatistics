197ca4ce41aa54ce004bd26d7a305b481bf0b554
ele20 <ele20@ic.ac.uk>
2021-06-28 18:51:03 +0100

    RF_Classifier stacked without updating external file now

--- data_extraction/bricks_dataloader/bricks_classifier_rf.py
+++ data_extraction/bricks_dataloader/bricks_classifier_rf.py
@@ -1,6 +1,7 @@
 import time
 
 from astropy.io import fits
+from astropy.table import Table, vstack
 import numpy as np
 import os
 import pandas as pd
@@ -45,7 +46,9 @@ df_stars = pd.DataFrame(columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
 print(df_galaxy.head())
 print(df_stars.head())
 
+base_table = Table.read(f'/Volumes/Astrostick/bricks_data/south/tractor-{bricknames_south_sample[-1]}.fits', format='fits')
 
+bricknames_south_sample.pop()
 
 for no, brickname in enumerate(bricknames_south_sample):
 
@@ -53,7 +56,42 @@ for no, brickname in enumerate(bricknames_south_sample):
 
         # Check how there can be a brickname without corresponding id
 
+    if no % 25 == 0:
+        if no == 0:
+            continue
+        hdu = fits.table_to_hdu(base_table)
 
+        data = hdu.data
+        brick = Brick(data)
+        brick.initialise_brick_for_galaxy_classification(south=True)
+        target_type = brick.classify_galaxies()
+
+        stacked_array = np.stack((brick.ids, brick.ra, brick.dec, target_type, brick.maskbits, brick.fitbits), axis=1)
+        support_df = pd.DataFrame(stacked_array, columns=['BrickID', 'RA', 'DEC', 'Target_type', 'Fitbits', 'Maskbits'])
+        support_df.drop(support_df[support_df.Target_type == 0].index, inplace=True)
+
+        df_galaxy = df_galaxy.append(support_df)
+
+        brick.initialise_brick_for_stellar_density()
+
+        stars = brick.get_stellar_objects()
+
+        support_df = pd.DataFrame(stars, columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
+        df_stars = df_stars.append(support_df)
+
+        # Initialise new table
+
+        base_table = Table.read(f'/Volumes/Astrostick/bricks_data/south/tractor-{brickname}.fits', format='fits')
+        continue
+        # new = Table.read('tractor-0053m395.fits', format='fits')
+
+    t2 = Table.read(f'/Volumes/Astrostick/bricks_data/south/tractor-{brickname}.fits', format='fits')
+    base_table = vstack([base_table, t2])
+
+    #header = fits.getheader(f'/Volumes/Astrostick/bricks_data/south_test/tractor-{brickname}.fits')
+    
+    #print("Brick progression ", time.time() - start)
+    """
     brickid = brickid_south[np.where(brickname_south == brickname)]
 
     #brickid = brickid_north[np.where(brickname_north == brickname)]
@@ -110,7 +148,7 @@ for no, brickname in enumerate(bricknames_south_sample):
     # ['BrickID', 'ObjectID','RA', 'DEC', 'South', 'Target_type']
 
     # df.to_csv('../bricks_data/galaxy_catalogue_sample_profiling.csv', index=False)
-
+    """
     if no % 20 == 0:
         print(no/200)
 
@@ -126,10 +164,45 @@ for no, brickname in enumerate(bricknames_south_sample):
         #df_stars = df_stars[0:0]
         break
 
+    """
+    print(df_galaxy.head())
+    print(df_galaxy.Maskbits.unique())
+    print(df_stars.head())
+
+    
+    print(df_galaxy.shape)
+    print(df_stars.shape)
+    """
 
     # print(" ===================== Brick", brickname, " complete=====================")
 
+"""
+hdu = fits.table_to_hdu(new)
+fits.update('tractor-0053m395.fits', hdu.data, 1)
 
+
+hdu = fits.open('tractor-0053m395.fits')
+print(hdu.info())
+data = hdu[1].data
+print(type(data))
+
+ra = data.field('ra')
+print(len(ra))
+brick = Brick(data)
+brick.initialise_brick_for_galaxy_classification(south=True)
+target_type = brick.classify_galaxies()
+
+
+
+# Process array
+stacked_array = np.stack((brick.ids, brick.ra, brick.dec, target_type, brick.maskbits, brick.fitbits), axis=1)
+support_df = pd.DataFrame(stacked_array, columns=['BrickID', 'RA', 'DEC', 'Target_type', 'Fitbits', 'Maskbits'])
+support_df.drop(support_df[support_df.Target_type == 0].insch0rf+
+ex, inplace=True)
+df_galaxy = df_galaxy.append(support_df)
+print(df_galaxy.shape)
+
+"""
 
 df_galaxy = df_galaxy.astype({'BrickID': 'int32', 'Target_type': 'int8', 'Fitbits': 'int16', 'Maskbits': 'int16'})
 
@@ -139,8 +212,22 @@ df_galaxy = df_galaxy.astype({'BrickID': 'int32', 'Target_type': 'int8', 'Fitbit
 df_galaxy.to_csv('../../bricks_data/galaxy_catalogue_sample_profiling.csv', index=False, header=False)
 df_stars.to_csv('../../bricks_data/stellar_catalogue_sample_profiling.csv', index=False, header=False)
 
+#print(df_galaxy.groupby('Target_type').count())
 
 
+"""
+
+data = hdul1[1].data
+brick = Brick(data)
+brick.initialise_brick_for_galaxy_classification(south=True)
+target_type = brick.classify_galaxies()
+
+# Process array
+stacked_array = np.stack((brick.ids, brick.ra, brick.dec, target_type, brick.maskbits, brick.fitbits), axis=1)
+support_df = pd.DataFrame(stacked_array, columns=['BrickID', 'RA', 'DEC', 'Target_type', 'Fitbits', 'Maskbits'])
+support_df.drop(support_df[support_df.Target_type == 0].index, inplace=True)
+df_galaxy = df_galaxy.append(support_df)
+"""
 print()
 print("=============================== Classification South Completed ==================================")
 print()
