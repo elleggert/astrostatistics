f586ae93b70632668ba45aa84515aa9e036c63bb
ele20 <ele20@ic.ac.uk>
2021-07-12 18:26:37 +0100

    Adopted pipeline to also take into account those

--- data_preprocessing/galaxy_classification_pipeline/pipeline.py
+++ data_preprocessing/galaxy_classification_pipeline/pipeline.py
@@ -11,6 +11,7 @@ from brick import Brick
 
 area = 'north'
 device = 'Astrodisk'
+bricks_to_classify = 5
 
 hdulistBricksSouthSummary = fits.open('../../bricks_data/survey-bricks-dr9-south.fits')
 data_south = hdulistBricksSouthSummary[1].data
@@ -26,7 +27,6 @@ survey_north = data_north.field('survey_primary')
 north_survey_is_south = np.invert(survey_north)
 
 start = time.time()
-
 
 print()
 print(f"=============================== Download {area} ..... ==================================")
@@ -45,23 +45,37 @@ for filename in os.listdir(f'/Volumes/{device}/bricks_data/{area}/'):
     brickn = brickn.replace(".fits", "")
     downloaded_bricks.append(brickn)
 
-
 # Getting a random sample of bricknames without replacement and deleting all that are already downloaded
 bricknames_sample = [x for x in bricknames if x not in downloaded_bricks]
+df = pd.read_csv(f'../../bricks_data/galaxy_catalogue_{area}.csv',
+                 dtype={'BrickID': 'int32', 'LRG': 'int8', 'ELG': 'int8', 'QSO': 'int8'})
 
-print(len(bricknames_sample))
+brickids_processed = list(df.BrickID.unique())
+
+bricknames_processed = []
+for i, id in enumerate(brickids_processed):
+    bricknames_processed.append(brickname_north[np.where(brickid_north == id)][0])
+
+# Deleted all those already downloaded
+bricknames_sample = [x for x in bricknames_sample if x not in bricknames_processed]
+
 df_galaxy = pd.DataFrame(columns=['BrickID', 'RA', 'DEC', 'LRG', 'ELG', 'QSO'])
 df_stars = pd.DataFrame(columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
 
 print(df_galaxy.head())
 print(df_stars.head())
 
+print(f"No of bricks left for area {area}: {len(bricknames_sample)} ")
+print(bricknames_sample)
+
+
 for i, brickname in enumerate(bricknames_sample):
     folder = brickname[:3]
     url = f'https://portal.nersc.gov/cfs/cosmo/data/legacysurvey/dr9/{area}/tractor/{folder}/tractor-{brickname}.fits'
     wget.download(url, f'/Volumes/{device}/bricks_data/{area}/')
 
-    #brickid = brickid_south[np.where(brickname_south == brickname)]
+    # brickid = brickid_south[np.where(brickname_south == brickname)]
+
     # North Bricks
     brickid = brickid_north[np.where(brickname_north == brickname)]
 
@@ -76,7 +90,7 @@ for i, brickname in enumerate(bricknames_sample):
 
     south = north_survey_is_south[np.where(brickid_north == brickid)]
 
-    #south = south_survey_is_south[np.where(brickid_south == brickid)]
+    # south = south_survey_is_south[np.where(brickid_south == brickid)]
     if len(south) > 0:
         south = south[0]
     else:
@@ -87,6 +101,10 @@ for i, brickname in enumerate(bricknames_sample):
 
     brick.initialise_brick_for_galaxy_classification(south)
     target_objects = brick.classify_galaxies()
+
+    # Appending one empty line per brick to be sure that all bricks are extracted
+    df_galaxy = df_galaxy.append({'BrickID': 630831, 'RA': np.nan, 'DEC': np.nan, 'LRG': 0, 'ELG': 0, 'QSO': 0},
+                                 ignore_index=True)
 
     support_df = pd.DataFrame(target_objects,
                               columns=['BrickID', 'RA', 'DEC', 'LRG', 'ELG', 'QSO'])
@@ -100,12 +118,6 @@ for i, brickname in enumerate(bricknames_sample):
     support_df = pd.DataFrame(stars, columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
     df_stars = df_stars.append(support_df)
 
-    # print("Brick progression ", time.time() - start)
-
-    # Do not forget to check this clause
-    # ['BrickID', 'ObjectID','RA', 'DEC', 'South', 'Target_type']
-
-    # df.to_csv('../bricks_data/galaxy_catalogue_sample_profiling.csv', index=False)
 
     if i % 488 == 0:
         print()
@@ -119,16 +131,23 @@ for i, brickname in enumerate(bricknames_sample):
         df_galaxy = df_galaxy[0:0]
         df_stars = df_stars[0:0]
 
-
+    # Remove Downloaded Brick
     os.remove(f'/Volumes/{device}/bricks_data/{area}/tractor-{brickname}.fits')
 
+    print(f" Brick {area} processed: ", brickname, ", Brick ", i, " of ", len(bricknames_sample))
+    if i > bricks_to_classify:
+        df_galaxy = df_galaxy.astype(
+            {'BrickID': 'int32', 'LRG': 'int8', 'ELG': 'int8', 'QSO': 'int8'})
+        df_galaxy.to_csv(f'../../bricks_data/galaxy_catalogue_{area}.csv', mode='a', index=False, header=False)
+        df_stars.to_csv(f'../../bricks_data/stellar_catalogue_{area}.csv', mode='a', index=False, header=False)
+        df_galaxy = df_galaxy[0:0]
+        df_stars = df_stars[0:0]
+        break
 
-    print(f" Brick {area} processed: ", brickname, ", Brick ", i , " of ", len(bricknames_sample))
 
 
 print()
 print(f"=============================== Download {area} completed ==================================")
 print()
 
-print("Time taken for: ", len(bricknames), " bricks: ", round(((time.time() - start)/60),2))
-
+print("Time taken for: ", len(bricknames), " bricks: ", round(((time.time() - start) / 60), 2))
