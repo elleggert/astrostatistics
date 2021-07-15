f11b81cf559a52fdbe4b1ce4a7f6b1c984f8564a
ele20 <ele20@ic.ac.uk>
2021-07-16 00:06:59 +0100

    Integrated into pipeline

--- data_preprocessing/galaxy_classification/pipeline.py
+++ data_preprocessing/galaxy_classification/pipeline.py
@@ -6,6 +6,7 @@ import time
 import pandas as pd
 from brick import Brick
 
+
 area = 'south'
 device = 'Astrodisk'
 bricks_to_classify = 1500
@@ -33,6 +34,9 @@ hdulistBricks = fits.open(f'../../bricks_data/survey-bricks-dr9-{area}.fits')
 data = hdulistBricks[1].data
 
 bricknames = list(data.field('brickname'))
+bricks = {}
+for brick in bricknames:
+    bricks[brick] = 1
 
 downloaded_bricks = []
 
@@ -42,8 +46,10 @@ for filename in os.listdir(f'/Volumes/{device}/bricks_data/{area}/'):
     brickn = brickn.replace(".fits", "")
     downloaded_bricks.append(brickn)
 
+for brick in downloaded_bricks:
+    if brick in bricks:
+        bricks.pop(brick)
 # Getting a random sample of bricknames without replacement and deleting all that are already downloaded
-bricknames_sample = [x for x in bricknames if x not in downloaded_bricks]
 df = pd.read_csv(f'../../bricks_data/galaxy_catalogue_{area}.csv',
                  dtype={'BrickID': 'int32', 'LRG': 'int8', 'ELG': 'int8', 'QSO': 'int8'})
 
@@ -54,15 +60,11 @@ print("Bricks processed:", len(brickids_processed))
 bricknames_processed = []
 for i, id in enumerate(brickids_processed):
     temp = brickname_south[np.where(brickid_south == id)]
-    #if len(temp) > 0:
     bricknames_processed.append(temp[0])
-    #else:
-        #print(id)
-        #print(temp)
 
-
-# Deleted all those already downloaded
-bricknames_sample = [x for x in bricknames_sample if x not in bricknames_processed]
+for brick in bricknames_processed:
+    if brick in bricks:
+        bricks.pop(brick)
 
 df_galaxy = pd.DataFrame(columns=['BrickID', 'RA', 'DEC', 'LRG', 'ELG', 'QSO'])
 df_stars = pd.DataFrame(columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
@@ -70,8 +72,9 @@ df_stars = pd.DataFrame(columns=['RA', 'DEC', 'GMAG', 'RMAG', 'ZMAG'])
 print(df_galaxy.head())
 print(df_stars.head())
 
+bricknames_sample = list(bricks.keys())
 print(f"No of bricks left for area {area}: {len(bricknames_sample)} ")
-print("Time taken for: ", round(((time.time() - start) / 60), 2))
+print("Time taken for bricks left extraction: ", round(((time.time() - start) / 60), 2))
 
 c = 0
 problem_bricks = []
