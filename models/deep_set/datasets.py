73c9113503281e21cba862fa48e1641b551648e1
ele20 <ele20@ic.ac.uk>
2021-07-19 18:03:43 +0100

    Experimented with DF Conversion of MiniMultidict to allow easier shuffle

--- models/deep_set/datasets.py
+++ models/deep_set/datasets.py
@@ -1,4 +1,7 @@
 import pickle
+
+from sklearn.model_selection import train_test_split
+
 from set_dataloader import CCD
 from torch.utils.data import DataLoader, Dataset
 import torch
@@ -138,24 +141,47 @@ class MultiSetSequence(Dataset):
     N = Number SubPixels of that are returned --> usually 64
     M = Max Size of each Individual Set of CCDs
     """
-    def __init__(self, num_pixels= 1000, num_subpixels = 64, max_ccds=30, num_features = 9):
+    def __init__(self, num_pixels = 1000, num_subpixels = 64, max_ccds=30, num_features = 9):
 
         with open('../../bricks_data/mini_multiset.pickle', 'rb') as f:
             self.mini_multiset = pickle.load(f)
             f.close()
 
-        # Initialise DataSet
-        self.num_pixels = num_pixels
         self.num_features = num_features
-        self.input = np.zeros((num_pixels, num_subpixels, max_ccds, num_features))
-        self.lengths = np.zeros((num_pixels,num_subpixels), dtype=int)
-        self.lrg = np.zeros(num_pixels)
-        self.elg = np.zeros(num_pixels)
-        self.qso = np.zeros(num_pixels)
+        self.num_subpixels = num_subpixels
+        self.max_ccds = max_ccds
+
+        self.keys = list(self.mini_multiset.keys())
+        random.shuffle(self.keys)
+        self.keys = self.keys[:num_pixels]
+
+        self.train_pix, self.test_pix = train_test_split(self.keys, test_size=0.33, random_state=44, shuffle=True)
+
+        # Initialise TrainData
+
+        self.initialise_traindata()
+
+        self.initialise_testdata()
 
         self.initialise_inputs()
 
-    def set_targets(self, gal_type):
+    def initialise_testdata(self):
+        self.test_num_pixels = len(self.test_pix)
+        self.test_input = np.zeros((self.test_num_pixels, self.num_subpixels, self.max_ccds, self.num_features))
+        self.test_lengths = np.zeros((self.test_num_pixels, self.num_subpixels), dtype=int)
+        self.test_lrg = np.zeros(self.test_num_pixels)
+        self.test_elg = np.zeros(self.test_num_pixels)
+        self.test_qso = np.zeros(self.test_num_pixels)
+
+    def initialise_traindata(self):
+        self.train_num_pixels = len(self.train_pix)
+        self.train_input = np.zeros((self.train_num_pixels, self.num_subpixels, self.max_ccds, self.num_features))
+        self.train_lengths = np.zeros((self.train_num_pixels, self.num_subpixels), dtype=int)
+        self.train_lrg = np.zeros(self.train_num_pixels)
+        self.train_elg = np.zeros(self.train_num_pixels)
+        self.train_qso = np.zeros(self.train_num_pixels)
+
+    def set_targets(self, gal_type, train):
         # Features and inputs:
         self.target = None
         if gal_type == 'lrg':
@@ -167,16 +193,25 @@ class MultiSetSequence(Dataset):
         self.scaler_out = preprocessing.MinMaxScaler()
         self.target = self.scaler_out.fit_transform(self.target.reshape(-1, 1))
 
-    def initialise_inputs(self):
-        for i, pix in enumerate(self.mini_multiset):
-            if i >= self.num_pixels:
+    def initialise_inputs_train(self):
+        for i, pix in enumerate(self.train_pix):
+            if i >= self.train_num_pixels:
                 break
-            self.input[i] = self.mini_multiset[pix][0]
-            self.lengths[i] = self.mini_multiset[pix][1]
-            self.lrg[i] = self.mini_multiset[pix][2]
-            self.elg[i] = self.mini_multiset[pix][3]
-            self.qso[i] = self.mini_multiset[pix][4]
+            self.train_input[i] = self.mini_multiset[pix][0]
+            self.train_lengths[i] = self.mini_multiset[pix][1]
+            self.train_lrg[i] = self.mini_multiset[pix][2]
+            self.train_elg[i] = self.mini_multiset[pix][3]
+            self.train_qso[i] = self.mini_multiset[pix][4]
 
+    def initialise_inputs_test(self):
+        for i, pix in enumerate(self.test_pix):
+            if i >= self.test_num_pixels:
+                break
+            self.test_input[i] = self.mini_multiset[pix][0]
+            self.test_lengths[i] = self.mini_multiset[pix][1]
+            self.test_lrg[i] = self.mini_multiset[pix][2]
+            self.test_elg[i] = self.mini_multiset[pix][3]
+            self.test_qso[i] = self.mini_multiset[pix][4]
 
     def __len__(self):
         return self.num_pixels
