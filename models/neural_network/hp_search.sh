3df37d889c0c1aae53d725ec410808b25f550c1d
Edgar Eggert <ele20@genie01.doc.ic.ac.uk>
2021-08-18 15:35:43 +0100

    Altered Hp_Search script to now run on ic gpus

--- models/neural_network/hp_search.sh
+++ models/neural_network/hp_search.sh
@@ -5,7 +5,7 @@ for area in north south des
 do
   for gal in lrg elg qso
     do
-      python hp_optim.py -g $gal -n 400 -a $area -t 10
+      python hp_optim.py -g $gal  -a $area -t 40
     done
 done
 
