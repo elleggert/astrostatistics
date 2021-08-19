b6ee5c8f7b7a10309581c2790ffd2dd9dc77703f
Edgar Eggert <ele20@gpu14.doc.ic.ac.uk>
2021-08-19 11:29:39 +0100

    smaller search bar, only look in south and des

--- models/neural_network/hp_search.sh
+++ models/neural_network/hp_search.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for area in north south des
+for area in south des
 do
   for gal in lrg elg qso
     do
-      python hp_optim.py -g $gal  -a $area -t 10 -n 400
+      python hp_optim.py -g $gal  -a $area -t 20 
     done
 done
 
