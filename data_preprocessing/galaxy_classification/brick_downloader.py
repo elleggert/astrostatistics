69d001fdecb15892df68d62ee46f36403d30d0b9
ele20 <ele20@ic.ac.uk>
2021-12-20 13:46:39 +0100

    Bug fix and style fix. Now the downloaded bricks are removed first and then we sample

--- data_preprocessing/galaxy_classification/brick_downloader.py
+++ data_preprocessing/galaxy_classification/brick_downloader.py
@@ -1,7 +1,6 @@
 from astropy.io import fits
 import os
 import wget
-import numpy.random
 import random
 import time
 import argparse
@@ -12,25 +11,26 @@ def main():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n', '--num_bricks', default=None, metavar='', type=int, help='number of bricks to download')
     parser.add_argument('-a', '--area', default='south', metavar='', type=str,
-                        help='The catalogue that ought to be downloaded, by default extracts southern bricks.')
+                        help='The catalogue that ought to be downloaded, by default extracts southern bricks. '
+                             'Accepted values: south or north')
 
     args = vars(parser.parse_args())
 
     # Timing information
-    #start = time.time()
+    # start = time.time()
 
     area = args['area']
     bricks_to_download = args['num_bricks']
     path = f'/Volumes/Astrostick/bricks_data/{area}/'
 
-    if area != 'south' or area != 'north':
+    if area != 'south' and area != 'north':
+        print(area)
         print("Invalid Area argument: must be either 'north' or 'south'")
         exit()
 
     print()
     print(f"=============================== Download {area} ..... ==================================")
     print()
-
 
     # Utilises the summary files to get a list of all bricks in the catalogue --> these need to be stored somewhere
     hdulistBricks = fits.open(f'../../bricks_data/survey-bricks-dr9-{area}.fits')
@@ -46,16 +46,24 @@ def main():
         brick = brick.replace(".fits", "")
         downloaded_bricks.append(brick)
 
-    print(f'Number of bricks left in {area}: {len(bricknames) - len(downloaded_bricks)})')
+    print(f'Number of bricks left in {area}: {len(bricknames) - len(downloaded_bricks)}')
 
     """If a numerical limit was provided, getting a random sample of bricks without replacement 
     and deleting all that are already downloaded, else simply iterate through all bricks"""
 
+    print(len(bricknames))
+
+    bricknames = [x for x in bricknames if x not in downloaded_bricks]
+    print(len(bricknames))
+
     if bricks_to_download:
         bricknames = random.sample(bricknames, bricks_to_download)
-    bricknames_sample = [x for x in bricknames if x not in downloaded_bricks]
+    print(len(bricknames))
 
-    for i, brickname in enumerate(bricknames_sample):
+
+    print(len(downloaded_bricks))
+    for i, brickname in enumerate(bricknames):
+        break
         folder = brickname[:3]
         url = f'https://portal.nersc.gov/cfs/cosmo/data/legacysurvey/dr9/{area}/tractor/{folder}/tractor-{brickname}.fits'
         wget.download(url, path)
@@ -64,7 +72,7 @@ def main():
     print(f"=============================== Download {area} completed ==================================")
     print()
 
-    #print("Time taken for: ", i, " bricks: ", round(((time.time() - start) / 60), 2))
+    # print("Time taken for: ", i, " bricks: ", round(((time.time() - start) / 60), 2))
 
     print(f"Number of bricks in downloaded in {area}:", len(os.listdir(path)))
 
