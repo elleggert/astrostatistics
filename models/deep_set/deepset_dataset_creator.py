6b8ff48fe5a12ba2e7c15fd30cb6803ff1b956d7
ele20 <ele20@ic.ac.uk>
2022-02-11 13:20:23 +0000

    Refactored the Data generation script

--- models/deep_set/deepset_dataset_creator.py
+++ models/deep_set/deepset_dataset_creator.py
@@ -10,7 +10,6 @@ import argparse
 
 # Importing Dicts and Defining Utilities
 
-
 def main():
     parser = argparse.ArgumentParser(description='MultiSetSequence Dataset Generator',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -22,66 +21,70 @@ def main():
     args = vars(parser.parse_args())
 
     NSIDE = 512
-    test = [False, True]
+    tests = [False, True]
+    export_to_disk = True
     area = args['area']
+    num_pixels = args['num_pixels']
 
-
-    time_start = time.time()
-
-    # Importing Pixel Mappings
-
-    # For DECAM, BASS, MzLS
-    with open('../../bricks_data/pixel2ccd_2048_non_inclusive.pickle', 'rb') as f:
-        subpixel2ccd_dict = pickle.load(f)
-        f.close()
-
-    time_end = time.time()
-    time_passed = time_end - time_start
-    print()
-    print(f"{time_passed / 60:.5} minutes ({time_passed:.3} seconds) taken to import the dict")
-    print()
-
-    time_start = time.time()
-
-    # For DECAM, BASS, MzLS
-    with open(f'../../bricks_data/pixel2subpixel_{NSIDE}_2048.pickle', 'rb') as f:
-        pixel2subpixel_dict = pickle.load(f)
-        f.close()
-
-    time_end = time.time()
-    time_passed = time_end - time_start
-    print()
-    print(f"{time_passed / 60:.5} minutes ({time_passed:.3} seconds) taken to import the dict")
-    print()
-
-    ccd = set_dataloader.CCD()
-
-    print(f'Number of Features : {ccd.num_features}')
-
-# Generate Train and Set
-
-
-    if test:
-        import_path = f'../../bricks_data/{area}_test_{NSIDE}.csv'
-        export_path = f'data/north/{area}_test_{NSIDE}.pickle'
-        # If stored on Astrodisk Volume
-        export_path = f'/Volumes/Astrodisk/bricks_data/{area}_test_{NSIDE}.pickle'
-
+    # Make this variable depenndent on the Area Size
+    if area == 'north':
+        max_ccds = 30
+    elif area == 'south':
+        max_ccds = 25
     else:
-        import_path = f'../../bricks_data/{area}_{NSIDE}.csv'
-        export_path = f'data/north/{area}_{NSIDE}.pickle'
-        # If stored on Astrodisk Volume
-        export_path = f'/Volumes/Astrodisk/bricks_data/{area}_{NSIDE}.pickle'
+        max_ccds = 50
 
-    df = pd.read_csv(import_path)
+    ccd, pixel2subpixel_dict, subpixel2ccd_dict = import_pixel_mappings(NSIDE)
 
-    num_pixels = len(df)
-    num_subpixels = 16
-    max_ccds = 30
-    num_features = ccd.num_features
-    # Randomly Sampling Pixel Indices from Dataframe
-    pix_ids = df.pixel_id.to_numpy()
-    # pix_ids = pix_ids[:num_pixels]
+    # Generate Train and Set
+
+    for test in tests:
+
+        if test:
+            import_path = f'../../bricks_data/{area}_test_{NSIDE}.csv'
+            if export_to_disk:
+                export_path = f'/Volumes/Astrodisk/bricks_data/{area}_test_{NSIDE}.pickle'
+            else:
+                export_path = f'data/north/{area}_test_{NSIDE}.pickle'
+
+
+        else:
+            import_path = f'../../bricks_data/{area}_{NSIDE}.csv'
+            if export_to_disk:
+                # If stored on Astrodisk Volume
+                export_path = f'/Volumes/Astrodisk/bricks_data/{area}_{NSIDE}.pickle'
+            else:
+                export_path = f'data/north/{area}_{NSIDE}.pickle'
+
+        df = pd.read_csv(import_path)
+        # Randomly Sampling Pixel Indices from Dataframe
+        pix_ids = df.pixel_id.to_numpy()
+
+        if num_pixels is None:
+            num_pixels = len(df)
+        else:
+            pix_ids = pix_ids[:num_pixels]
+        num_subpixels = 16
+        num_features = ccd.num_features
+
+        input, lengths = generate_length_inputs(ccd, max_ccds, num_features, num_pixels, num_subpixels, pix_ids,
+                                                pixel2subpixel_dict, subpixel2ccd_dict)
+
+        # Generate Dict
+
+        mini_multiset = generate_dict(df, input, lengths, pix_ids)
+
+        with open(export_path, 'wb') as f:
+            pickle.dump(mini_multiset, f)
+            f.close()
+
+        """with open(export_path, 'wb') as f:
+            pickle.dump(mini_multiset, f)
+            f.close()"""
+
+
+def generate_length_inputs(ccd, max_ccds, num_features, num_pixels, num_subpixels, pix_ids, pixel2subpixel_dict,
+                           subpixel2ccd_dict):
     input = np.zeros((num_pixels, num_subpixels, max_ccds, num_features))
     # Iterate through the pixels
     for pix_no, pix in enumerate(pix_ids):
@@ -119,27 +122,21 @@ def main():
 
     else:
         lengths.fill(max_ccds)
+    return input, lengths
 
-    print(pix_ids.shape)
-    print(input.shape)
-    print(lengths.shape)
-    print(input[1])
 
-    # Generate Dict
-
+def generate_dict(df, input, lengths, pix_ids):
     lrg = df.lrg.to_numpy()
     elg = df.elg.to_numpy()
     qso = df.qso.to_numpy()
     glbg = df.glbg.to_numpy()
     rlbg = df.rlbg.to_numpy()
-
     stellar = df.stellar.to_numpy().flatten()
     ebv = df.EBV.to_numpy().flatten()
     hinh = df.hinh.to_numpy().flatten()
     gaia = df.gaia.to_numpy().flatten()
     gaia12 = df.gaia12.to_numpy().flatten()
     sagitarius = df.sagitarius.to_numpy().flatten()
-
     mini_multiset = defaultdict(list)
     for i, pix in enumerate(pix_ids):
         if i % 200 == 0:
@@ -158,14 +155,34 @@ def main():
         mini_multiset[pix].append(sagitarius[i])
         mini_multiset[pix].append(ebv[i])
         mini_multiset[pix].append(pix)
+    return mini_multiset
 
-    with open(export_path, 'wb') as f:
-        pickle.dump(mini_multiset, f)
+
+def import_pixel_mappings(NSIDE):
+    time_start = time.time()
+    # Importing Pixel Mappings
+    # For DECAM, BASS, MzLS
+    with open('../../bricks_data/pixel2ccd_2048_non_inclusive.pickle', 'rb') as f:
+        subpixel2ccd_dict = pickle.load(f)
         f.close()
-
-    """with open(export_path, 'wb') as f:
-        pickle.dump(mini_multiset, f)
-        f.close()"""
+    time_end = time.time()
+    time_passed = time_end - time_start
+    print()
+    print(f"{time_passed / 60:.5} minutes ({time_passed:.3} seconds) taken to import the dict")
+    print()
+    time_start = time.time()
+    # For DECAM, BASS, MzLS
+    with open(f'../../bricks_data/pixel2subpixel_{NSIDE}_2048.pickle', 'rb') as f:
+        pixel2subpixel_dict = pickle.load(f)
+        f.close()
+    time_end = time.time()
+    time_passed = time_end - time_start
+    print()
+    print(f"{time_passed / 60:.5} minutes ({time_passed:.3} seconds) taken to import the dict")
+    print()
+    ccd = set_dataloader.CCD()
+    print(f'Number of Features : {ccd.num_features}')
+    return ccd, pixel2subpixel_dict, subpixel2ccd_dict
 
 
 if __name__ == "__main__":
