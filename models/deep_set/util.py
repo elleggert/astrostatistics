533790b13c7a02289ae872df43f8607106d6e158
ele20 <ele20@ic.ac.uk>
2022-02-24 19:24:44 +0000

    Deleting both unused get dataset utilities

--- models/deep_set/util.py
+++ models/deep_set/util.py
@@ -11,34 +11,6 @@ from datasets import MultiSetSequence
 def get_mask(sizes, max_size):
     return (torch.arange(max_size).reshape(1, -1).to(sizes.device) < sizes.unsqueeze(2))
 
-
-def get_dataset(num_pixels, max_set_len, gal, path_to_data='data/multiset.pickle'):
-    with open(path_to_data, 'rb') as f:
-        mini_multiset = pickle.load(f)
-        f.close()
-    df = pd.DataFrame.from_dict(mini_multiset, orient='index')
-    zscore = lambda x: abs((x - x.median()) / x.std())
-    df['Z_LRG'] = df[2].transform(zscore)
-    df['Z_ELG'] = df[3].transform(zscore)
-    df['Z_QSO'] = df[4].transform(zscore)
-    df = df[(df['Z_LRG'] < 3)]
-    df = df[(df['Z_ELG'] < 3)]
-    df = df[(df['Z_QSO'] < 3)]
-    num = num_pixels
-    if num > len(df):
-        num = len(df)
-    df.drop(columns=['Z_ELG', 'Z_LRG', 'Z_QSO'], inplace=True)
-    train_df, test_df = train_test_split(df, test_size=0.33, random_state=44, shuffle=True)
-    traindata = MultiSetSequence(dict=train_df.to_dict(orient='index'), num_pixels=round(num * 0.67),
-                                 max_ccds=max_set_len)
-    traindata.set_targets(gal_type=gal)
-
-    testdata = MultiSetSequence(dict=test_df.to_dict(orient='index'), num_pixels=round(num * 0.33),
-                                max_ccds=max_set_len)
-    testdata.set_targets(gal_type=gal, scaler=traindata.scaler)
-
-    return traindata, testdata
-
 def get_full_dataset(area, num_pixels, max_set_len, gal):
     print(f"Starting Loading {area}")
     with open(f'data/{area}/{area}_512_robust.pickle', 'rb') as f:
