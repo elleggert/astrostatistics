87b31c3295d74c1f516f1be1c06d6b85ac488dd5
Edgar Eggert <ele20@genie01.doc.ic.ac.uk>
2022-03-14 09:58:47 +0000

    Weekened runs

--- models/deep_set/RUNME.sh
+++ models/deep_set/RUNME.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./hp_optim.sh |& tee logs_figs/north/elg_8_dual.txt
+./hp_optim.sh |& tee logs_figs/rlbg_11_dual.txt
--- models/deep_set/hp_optim.sh
+++ models/deep_set/hp_optim.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-for area in north
+for area in south north des
 do
-  for gal in elg
+  for gal in rlbg
     do
 	python hp_optim.py -g $gal -a $area -t 15
 	python fine_tune.py -g $gal -a $area -t 15
